d0a92d0fd1159c7faec6842003e273bfcc27f602
Andrew Glyadchenko <andrewgly95@gmail.com>
2018-06-01 11:37:13 -0400

    Added playurl function

--- Jukebox.js
+++ Jukebox.js
@@ -7,12 +7,18 @@ function Pause() {
   document.querySelector(".example").pause();
 }
 
-function Add() {
-  var songUrl = document.querySelector("addurl").value;
+function PlayUrl() {
+  var songUrl = document.querySelector("#playurl").value;
+  document.querySelector(".example").src = songUrl;
+  Play();
 
 }
 
 function Jukebox() {
   this.audioSources = ["https://www.bensound.com/bensound-music/bensound-betterdays.mp3","https://www.bensound.com/bensound-music/bensound-dubstep.mp3"];
   this.audioPlayer = document.querySelector(".example");
+  this.addSong = function(url) {
+    audioPlayer.src = url;
+    audioPlayer.play();
+  }
 }
