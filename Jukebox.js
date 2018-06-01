5bf5dd9e5dff51a24ab8848a32fc8e68bac05b45
Andrew Glyadchenko <andrewgly95@gmail.com>
2018-06-01 12:05:06 -0400

    Added next, add, play

--- Jukebox.js
+++ Jukebox.js
@@ -1,24 +1,46 @@
 function Play() {
-  document.querySelector(".example").play();
-  console.log("play called");
+  myJukebox.play();
 }
 
 function Pause() {
-  document.querySelector(".example").pause();
+  myJukebox.pause();
+}
+
+function Next() {
+  myJukebox.nextSong();
 }
 
 function PlayUrl() {
   var songUrl = document.querySelector("#playurl").value;
-  document.querySelector(".example").src = songUrl;
-  Play();
+  myJukebox.playSong(songUrl);
+}
 
+function AddUrl() {
+  var songUrl = document.querySelector("#playurl").value;
+  myJukebox.addSong(songUrl);
 }
 
 function Jukebox() {
   this.audioSources = ["https://www.bensound.com/bensound-music/bensound-betterdays.mp3","https://www.bensound.com/bensound-music/bensound-dubstep.mp3"];
-  this.audioPlayer = document.querySelector(".example");
-  this.addSong = function(url) {
-    audioPlayer.src = url;
-    audioPlayer.play();
+  this.audioPlayer = document.querySelector(".player");
+  this.currentSong = 0;
+  this.audioPlayer.src = this.audioSources[this.currentSong];
+  this.play = function() {
+    this.audioPlayer.play();
+  }
+  this.pause = function() {
+    this.audioPlayer.pause();
+  }
+  this.playSong = function(url) {
+    this.audioPlayer.src = url;
+    this.audioPlayer.play();
+  }
+  this.nextSong = function(){
+    this.audioPlayer.src = this.audioSources[++this.currentSong];
+  }
+  this.addSong = function(url){
+    this.audioSources.push(url);
   }
 }
+
+var myJukebox = new Jukebox();
